d2219150062e812847861665acf6829e9e4033f7
Colson <colsoncalifornication@gmail.com>
2024-05-11 16:03:14 +0545

    add links between pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,9 @@
 // This index.jsx will be server if a request reaches our domain slash nothing
 // For Example: our-domain.com/
 
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 // What it goes in these page files is, our standard React Component, the React
 // component that should be loaded for that specific page.
 // We must export this component so that NEXT js is able to find it
@@ -9,7 +12,21 @@
 // projects support this modern React setup where we can omit this import and
 // it's added behind the scenes, so to say.
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-great-framework'>
+            NEXTjs is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href='/news/silicon-valley'>Silicon Valley</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default HomePage;
