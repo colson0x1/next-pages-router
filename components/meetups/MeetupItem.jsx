dccbd24326047619d6643e7bbdde00efb608c11c
Colson <colsoncalifornication@gmail.com>
2024-05-11 20:45:42 +0545

    use programmatic (imperative) navigation

--- components/meetups/MeetupItem.jsx
+++ components/meetups/MeetupItem.jsx
@@ -1,7 +1,20 @@
+import { useRouter } from 'next/router';
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const router = useRouter();
+
+  function showDetailsHandler() {
+    // The pushes a new page onto the stack of pages and it's the equivalent of
+    // using the Link component.
+    // If we don't want a link but instead navigate progrmatically, we use
+    // push method.
+    // push use a path to which we wanna navigate!
+    router.push('/' + props.id);
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +26,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
